dc55fbe379cb0e7d1289db13bd230798afaf9209
Saad Ali Malik <saad.malik6366@gmail.com>
2024-04-05 19:57:00 +0500

    Add wish list styling and import necessary modules

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { WishItem } from '../shared/models/wishItem';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'wishlist';
+  items: WishItem[] = [
+    new WishItem('Start development journey', true),
+    new WishItem('Learn Frontend frameworks'),
+    new WishItem('Get really good at programming'),
+    new WishItem('Become an accomplished individual')
+  ]
+  
+  toggleItem(item: WishItem) {
+    item.isComplete = !item.isComplete;
+  }
 }
