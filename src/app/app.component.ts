f96e9ad7f187f7297127d6c771fd21a963bd7858
Saad Ali Malik <saad.malik6366@gmail.com>
2024-04-07 17:44:53 +0500

    Refactor wish list UI and functionality, and update import statements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,40 +1,29 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { WishItem } from '../shared/models/wishItem';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 import { WishListComponent } from './wish-list/wish-list.component';
 import { AddWishFormComponent } from './add-wish-form/add-wish-form.component';
-
-const filters = [
-  (item: WishItem) => item,
-  (item: WishItem) => !item.isComplete,
-  (item: WishItem) => item.isComplete,
-];
+import { WishFilterComponent } from './wish-filter/wish-filter.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    RouterOutlet,
-    CommonModule,
-    FormsModule,
-    WishListComponent,
-    AddWishFormComponent,
-  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
+  imports: [
+    RouterOutlet,
+    WishListComponent,
+    AddWishFormComponent,
+    WishFilterComponent,
+  ],
 })
 export class AppComponent {
   items: WishItem[] = [
-    new WishItem('Start development journey', true),
-    new WishItem('Learn Frontend frameworks'),
-    new WishItem('Get really good at programming'),
-    new WishItem('Become an accomplished individual'),
+    // new WishItem('Start development journey', true),
+    // new WishItem('Learn Frontend frameworks'),
+    // new WishItem('Get really good at programming'),
+    // new WishItem('Become an accomplished individual'),
   ];
-  listFilter: any = '0';
 
-  get visibleItems(): WishItem[] {
-    return this.items.filter(filters[this.listFilter]);
-  }
+  listFilter: any = () => {};
 }
